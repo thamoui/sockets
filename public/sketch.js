c7cb5a55103b4f6843fa6367480ba846c4bb6deb
thamoui <hamoui.antoine@gmail.com>
2016-08-01 10:45:26 +0200

    1 commit

---
+++ public/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+    createCanvas(900, 600);
+    background(51);
+    
+    socket = io.connect('localhost:3000');
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data){
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x, data.y, 22, 22);  
+    
+}
+
+function mouseDragged(){
+    console.log('sending : ' + mouseX + ',' + mouseY);
+    var data = {
+       x: mouseX,
+       y: mouseY
+    }
+    
+    socket.emit('mouse', data);
+    
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 22, 22);
+}
+
+function draw(){
+    
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+
+
+ console.log("test my node js server on port 3000");
+ 
+ var socket = require('socket.io');
+ 
+ var io = socket(server);
+ io.sockets.on('connection', newConnection);
+ 
+ function newConnection(socket){
+     console.log('new connection: ' + socket.id);
+     
+     socket.on('mouse', mouseMsg);
+     
+     function mouseMsg(data) {
+         socket.broadcast.emit('mouse', data);
+         // io.socket.emit('mose', data);
+         console.log(data);
+     } 
+ }
