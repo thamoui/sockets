4a15eabe244f354e2193324c71398928f22bbeba
thamoui <hamoui.antoine@gmail.com>
2016-08-02 14:25:11 +0200

    test6

--- server.js
+++ server.js
@@ -3,23 +3,24 @@ var express = require('express');
 
 var app = express();
 
+// var server = app.listen(3000);
+
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-// server.listen(server_port, server_ip_address, function () {
-//   console.log( "Listening on " + server_ip_address + ", server_port " + port )
-// });
+var server = app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
 
-
-
-
- var server = app.listen(server_port);
+ 
 
  app.use(express.static('app'));
  
  var socket = require('socket.io');
  
  var io = socket(server);
+ 
  io.sockets.on('connection', newConnection);
  
  function newConnection(socket){
