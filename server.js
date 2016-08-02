9855a06de6bca647188627833b327e48c4586c54
thamoui <hamoui.antoine@gmail.com>
2016-08-02 13:08:04 +0200

    test5

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(8080);
 
-app.use(express.static('app'));
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+// server.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", server_port " + port )
+// });
 
 
 
- console.log("test my node js server on port 8080 ");
+
+ var server = app.listen(server_port);
+
+ app.use(express.static('app'));
  
  var socket = require('socket.io');
  
